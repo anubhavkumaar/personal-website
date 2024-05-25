ed94606fda291bd32aa89b298d65576a99d69b7e
Aveg Ganorkar <54445464+DarakhTech@users.noreply.github.com>
2024-05-25 16:58:16 +0530

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  images: {
+    domains: [
+      'res.cloudinary.com',
+      'avatars.githubusercontent.com',
+      'raw.githubusercontent.com',
+      'i.imgur.com',
+      'imgur.com',
+      'raw.githubusercontent.com',
+      'www.vectorlogo.zone',
+      'www.crummy.com',
+      'seaborn.pydata.org',
+      'upload.wikimedia.org',
+      
+    ],
+  },
+};
